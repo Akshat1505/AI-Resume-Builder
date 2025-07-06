6c0c31990de866151c91eecf9af72ae775e0ce12
akshat <gameratom15052005@gmail.com>
2025-07-07 00:40:34 +0530

    auth

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import React from 'react'
+import './App.css'
+import { Navigate, Outlet } from 'react-router-dom'
+import { useUser } from '@clerk/clerk-react';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const {user,isLoaded,isSignedIn}= useUser();
+
+  if(!isSignedIn && isLoaded){
+    return <Navigate to={'auth/sign-in'}/>
+  }
+
+
+  return (
+    <>
+      <Outlet/>
+    </>
+  )
+}
+
+export default App
