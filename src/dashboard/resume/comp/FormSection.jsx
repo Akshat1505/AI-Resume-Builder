dfb9d46cac1610426e5ba9eaad47d3ce8cce3079
akshat <gameratom15052005@gmail.com>
2025-07-17 23:28:21 +0530

    update5

--- src/dashboard/resume/comp/FormSection.jsx
+++ src/dashboard/resume/comp/FormSection.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { act, useState } from 'react'
+import Detail from './forms/Detail'
+import { Button } from '@/components/ui/button'
+import { ArrowLeft, ArrowRight, LayoutGrid } from 'lucide-react'
 
 function FormSection() {
+
+  const [active,setActive] = useState(1);
+  const [enableNext,setEnableNext] = useState(false);
   return (
-    <div>FormSection</div>
+    <div>
+      <div className='flex justify-around'>
+        <Button className="flex gap-2" size="sm" variant="outline"><LayoutGrid/>Theme</Button>
+        <div className='flex gap-2'>
+          {active>1&&<Button size="sm" onClick={()=>setActive(active-1)} className=""><ArrowLeft/></Button>}
+          <Button disabled={!enableNext} className="flex gap-2" size="sm" onClick={()=>setActive(active+1)}>Next<ArrowRight/></Button>
+        </div>
+      </div>
+      {/* Personal Detail */}
+      {active==1? <Detail enableNext={(v)=>setEnableNext(v)}/>:null}
+      {/* Summary */}
+
+      {/* Exp */}
+
+      {/* Edu */}
+
+      {/* SKills */}
+    </div>
   )
 }
 
